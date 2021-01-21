b297e1284f1e135e64b448e4fd9e221bd8e764bc
rhkdqhrehddl <rhkdqhrehddl@gmail.com>
2021-01-21 13:46:51 +0900

    모바일 빌링결제 모듈 returnurl 경로 수정

--- SmartXPayBilling/SmartXpay_CardBillingAuth.php
+++ SmartXPayBilling/SmartXpay_CardBillingAuth.php
@@ -27,8 +27,7 @@
 	/*
 	 * LGD_RETURNURL 을 설정하여 주시기 바랍니다. 반드시 현재 페이지와 동일한 프로트콜 및  호스트이어야 합니다. 아래 부분을 반드시 수정하십시요.
 	 */    
-	$LGD_RETURNURL				= "https://" . $server_domain . "/SmartXPayBilling/returnurl.jsp";  
-
+	$LGD_RETURNURL				= "https://" . $server_domain . "/SmartXPayBilling/returnurl.php";  
 	$LGD_PAYWINDOWTYPE    		= "CardBillingAuth_smartphone";
 	$LGD_VERSION				= "PHP_SmartXPay_CardBilling";
 
