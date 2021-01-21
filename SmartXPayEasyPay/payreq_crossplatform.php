a458828d7f477add885226fabfddb022ffd1ca23
rhkdqhrehddl <rhkdqhrehddl@gmail.com>
2021-01-21 14:56:48 +0900

    모바일 간편결제 모듈 연동

--- SmartXPayEasyPay/payreq_crossplatform.php
+++ SmartXPayEasyPay/payreq_crossplatform.php
@@ -12,21 +12,24 @@
      * 
      * 기본정보를 변경하여 주시기 바랍니다.(파라미터 전달시 POST를 사용하세요)
      */
-    $CST_PLATFORM               = $_POST["CST_PLATFORM"];				//토스페이먼츠 결제 서비스 선택(test:테스트, service:서비스)
-    $CST_MID                    = $_POST["CST_MID"];					//상점아이디(토스페이먼츠으로 부터 발급받으신 상점아이디를 입력하세요)
-                                                                        //테스트 아이디는 't'를 반드시 제외하고 입력하세요.
-    $LGD_MID                    = (("test" == $CST_PLATFORM)?"t":"").$CST_MID;  //상점아이디(자동생성)
-    $LGD_OID                    = $_POST["LGD_OID"];					//주문번호(상점정의 유니크한 주문번호를 입력하세요)
-    $LGD_AMOUNT                 = $_POST["LGD_AMOUNT"];					//결제금액("," 를 제외한 결제금액을 입력하세요)
-    $LGD_BUYER                  = $_POST["LGD_BUYER"];					//구매자명
-    $LGD_PRODUCTINFO            = $_POST["LGD_PRODUCTINFO"];			//상품명
-    $LGD_BUYEREMAIL             = $_POST["LGD_BUYEREMAIL"];				//구매자 이메일
-    $LGD_CUSTOM_FIRSTPAY        = $_POST["LGD_CUSTOM_FIRSTPAY"];		//상점정의 초기결제수단
-    $LGD_PCVIEWYN				= $_POST["LGD_PCVIEWYN"];				//휴대폰번호 입력 화면 사용 여부(유심칩이 없는 단말기에서 입력-->유심칩이 있는 휴대폰에서 실제 결제)
-    $LGD_CUSTOM_SKIN            = "SMART_XPAY2";                        //상점정의 결제창 스킨
-    $LGD_CUSTOM_USABLEPAY       = $_POST["LGD_CUSTOM_USABLEPAY"];
 	$server_domain = $_SERVER['HTTP_HOST'];
-    
+    $CST_PLATFORM               = $_POST["CST_PLATFORM"];						//토스페이먼츠 결제 서비스 선택(test:테스트, service:서비스)
+    $CST_MID                    = $_POST["CST_MID"];							//상점아이디(토스페이먼츠으로 부터 발급받으신 상점아이디를 입력하세요)
+																				//테스트 아이디는 't'를 반드시 제외하고 입력하세요.
+    $LGD_MID                    = (("test" == $CST_PLATFORM)?"t":"").$CST_MID;  //상점아이디(자동생성)
+    $LGD_OID                    = $_POST["LGD_OID"];							//주문번호(상점정의 유니크한 주문번호를 입력하세요)
+    $LGD_BUYER                  = $_POST["LGD_BUYER"];							//구매자명
+    $LGD_BUYEREMAIL             = $_POST["LGD_BUYEREMAIL"];						//구매자 이메일
+	$LGD_PRODUCTINFO            = $_POST["LGD_PRODUCTINFO"];					//상품명
+    $LGD_AMOUNT                 = $_POST["LGD_AMOUNT"];							//결제금액("," 를 제외한 결제금액을 입력하세요)
+	$LGD_CUSTOM_FIRSTPAY        = $_POST["LGD_CUSTOM_FIRSTPAY"];				//상점정의 초기결제수단
+	$LGD_INSTALLRANGE			= $_POST["LGD_INSTALLRANGE"];					//할부개월
+	$LGD_USABLECARD				= $_POST["LGD_USABLECARD"];						//결제 신용카드
+	$LGD_EASYPAY_ONLY           = $_POST["LGD_EASYPAY_ONLY"];					//paynow 사용여부
+	$LGD_MONEPAY_AUTORUNYN      = $_POST["LGD_MONEPAY_AUTORUNYN"];				//paynow 자동구동 여부
+	$LGD_PCVIEWYN				= $_POST["LGD_PCVIEWYN"];						//휴대폰번호 입력 화면 사용 여부(유심칩이 없는 단말기에서 입력-->유심칩이 있는 휴대폰에서 실제 결제)
+	
+	
 	$configPath 				= "C:/lgdacom"; 						//토스페이먼츠에서 제공한 환경파일("/conf/lgdacom.conf") 위치 지정. 	    
     
 	if(PHP_OS === "Linux"){
@@ -40,23 +43,22 @@
     /*
      * LGD_RETURNURL 을 설정하여 주시기 바랍니다. 반드시 현재 페이지와 동일한 프로트콜 및  호스트이어야 합니다. 아래 부분을 반드시 수정하십시요.
      */    
-    $LGD_RETURNURL				= "https://" . $server_domain . "/SmartXPay/returnurl.php";  
+    $LGD_RETURNURL				= "https://" . $server_domain . "/SmartXPayEasyPay/returnurl.php";  
 	
-	/*
-	* ISP 카드결제 연동을 위한 파라미터(필수)
-	*/
-	$LGD_KVPMISPWAPURL		= "";
-	$LGD_KVPMISPCANCELURL   = "";
-	
+    /*
+     * ISP 카드결제 연동을 위한 파라미터(필수)
+     */    
+    $LGD_KVPMISPWAPURL			= ""; 
+    $LGD_KVPMISPCANCELURL     	= "";
+    
 	$LGD_MPILOTTEAPPCARDWAPURL = ""; //iOS 연동시 필수
-	   
+
 	/*
 	* 계좌이체 연동을 위한 파라미터(필수)
 	*/
 	$LGD_MTRANSFERWAPURL 		= "";
 	$LGD_MTRANSFERCANCELURL 	= "";   
-	   
-    
+
     /*
      *************************************************
      * 2. MD5 해쉬암호화 (수정하지 마세요) - BEGIN
@@ -75,15 +77,14 @@
      * 토스페이먼츠에서 발급한 상점키(MertKey)를 환경설정 파일(lgdacom/conf/mall.conf)에 반드시 입력하여 주시기 바랍니다.
      */
     require_once($configPath . "/XPayClient.php");
-    $xpay = new XPayClient($configPath, $LGD_PLATFORM);
+    $xpay = new XPayClient($configPath, $CST_PLATFORM);
    	if (!$xpay->Init_TX($LGD_MID)) {
         echo "토스페이먼츠에서 제공한 환경파일이 정상적으로 설치 되었는지 확인하시기 바랍니다.<br/>";
         echo "mall.conf에는 Mert Id = Mert Key 가 반드시 등록되어 있어야 합니다.<br/><br/>";
         echo "문의전화 토스페이먼츠 1544-7772<br/>";
         exit;
-    }
-	
-	$LGD_TIMESTAMP = $xpay->GetTimeStamp(); 
+    }   
+    $LGD_TIMESTAMP = $xpay->GetTimeStamp(); 
     $LGD_HASHDATA = $xpay->GetHashData($LGD_MID,$LGD_OID,$LGD_AMOUNT,$LGD_TIMESTAMP);
     $LGD_CUSTOM_PROCESSTYPE = "TWOTR";
     /*
@@ -91,51 +92,56 @@
      * 2. MD5 해쉬암호화 (수정하지 마세요) - END
      *************************************************
      */
-    $CST_WINDOW_TYPE = "submit";										// 수정불가
-    $payReqMap['CST_PLATFORM']           = $CST_PLATFORM;				// 테스트, 서비스 구분
-    $payReqMap['CST_WINDOW_TYPE']        = $CST_WINDOW_TYPE;			// 수정불가
-    $payReqMap['CST_MID']                = $CST_MID;					// 상점아이디
-    $payReqMap['LGD_MID']                = $LGD_MID;					// 상점아이디
-    $payReqMap['LGD_OID']                = $LGD_OID;					// 주문번호
-    $payReqMap['LGD_BUYER']              = $LGD_BUYER;            		// 구매자
-    $payReqMap['LGD_PRODUCTINFO']        = $LGD_PRODUCTINFO;     		// 상품정보
-    $payReqMap['LGD_AMOUNT']             = $LGD_AMOUNT;					// 결제금액
-    $payReqMap['LGD_BUYEREMAIL']         = $LGD_BUYEREMAIL;				// 구매자 이메일
-    $payReqMap['LGD_CUSTOM_SKIN']        = $LGD_CUSTOM_SKIN;			// 결제창 SKIN
-    $payReqMap['LGD_CUSTOM_PROCESSTYPE'] = $LGD_CUSTOM_PROCESSTYPE;		// 트랜잭션 처리방식
-    $payReqMap['LGD_TIMESTAMP']          = $LGD_TIMESTAMP;				// 타임스탬프
-    $payReqMap['LGD_HASHDATA']           = $LGD_HASHDATA;				// MD5 해쉬암호값
-    $payReqMap['LGD_RETURNURL']   		 = $LGD_RETURNURL;      		// 응답수신페이지
-    $payReqMap['LGD_VERSION']         	 = "PHP_Non-ActiveX_SmartXPay";	// 버전정보 (삭제하지 마세요)
-    $payReqMap['LGD_CUSTOM_FIRSTPAY']  	 = $LGD_CUSTOM_FIRSTPAY;		// 디폴트 결제수단
-	$payReqMap['LGD_PCVIEWYN']			 = $LGD_PCVIEWYN;				// 휴대폰번호 입력 화면 사용 여부(유심칩이 없는 단말기에서 입력-->유심칩이 있는 휴대폰에서 실제 결제)
-    $payReqMap['LGD_CUSTOM_SWITCHINGTYPE']  = "SUBMIT";					// 신용카드 카드사 인증 페이지 연동 방식
-    $payReqMap['LGD_CUSTOM_USABLEPAY']     = $LGD_CUSTOM_USABLEPAY;
+
+    $CST_WINDOW_TYPE = "submit";													// 수정불가
+    $payReqMap['CST_PLATFORM']				= $CST_PLATFORM;						// 테스트, 서비스 구분
+    $payReqMap['CST_MID']					= $CST_MID;								// 상점아이디
+    $payReqMap['LGD_MID']					= $LGD_MID;								// 상점아이디
+    $payReqMap['LGD_OID']					= $LGD_OID;								// 주문번호
+    $payReqMap['LGD_HASHDATA']				= $LGD_HASHDATA;						// MD5 해쉬암호값
+    $payReqMap['LGD_BUYER']					= $LGD_BUYER;							// 구매자
+    $payReqMap['LGD_BUYEREMAIL']            = $LGD_BUYEREMAIL;                      // 구매자 이메일
+    $payReqMap['LGD_PRODUCTINFO']			= $LGD_PRODUCTINFO;						// 상품정보
+    $payReqMap['LGD_AMOUNT']				= $LGD_AMOUNT;							// 결제금액
+    $payReqMap['LGD_TIMESTAMP']				= $LGD_TIMESTAMP;						// 타임스탬프
+    $payReqMap['LGD_CUSTOM_FIRSTPAY']  		= $LGD_CUSTOM_FIRSTPAY;					// 디폴트 결제수단
+    $payReqMap['LGD_CUSTOM_PROCESSTYPE']	= $LGD_CUSTOM_PROCESSTYPE;				// 트랜잭션 처리방식
+    $payReqMap['CST_WINDOW_TYPE']           = $CST_WINDOW_TYPE;                     // 수정불가
+    $payReqMap['LGD_RETURNURL']   			= $LGD_RETURNURL;						// 응답수신페이지
+    $payReqMap['LGD_VERSION']         		= "PHP_Non-ActiveX_SmartXPay_Paynow";	// 사용타입 정보(수정 및 삭제 금지): 이 정보를 근거로 어떤 서비스를 사용하는지 판단할 수 있습니다.
+    $payReqMap['LGD_CUSTOM_SWITCHINGTYPE']  = "SUBMIT";								// 신용카드 카드사 인증 페이지 연동 방식     
+    $payReqMap['LGD_DOMAIN_URL'] 			= "xpayvvip";	
 	
-	
+    $payReqMap['LGD_INSTALLRANGE']			= $LGD_INSTALLRANGE;					// 할부개월
+    $payReqMap['LGD_USABLECARD']			= $LGD_USABLECARD;						// 결제 신용카드
+    $payReqMap['LGD_EASYPAY_ONLY']			= $LGD_EASYPAY_ONLY;                    // paynow 사용여부
+    $payReqMap['LGD_MONEPAY_AUTORUNYN']		= $LGD_MONEPAY_AUTORUNYN;				// paynow 자동구동 여부
+	$payReqMap['LGD_PCVIEWYN']				= $LGD_PCVIEWYN;						// 휴대폰번호 입력 화면 사용 여부(유심칩이 없는 단말기에서 입력-->유심칩이 있는 휴대폰에서 실제 결제)
+
+
 	//iOS 연동시 필수
 	$payReqMap['LGD_MPILOTTEAPPCARDWAPURL'] = $LGD_MPILOTTEAPPCARDWAPURL;
-  
-	/*
+
+    /*
 	****************************************************
 	* 신용카드 ISP(국민/BC)결제에만 적용 - BEGIN 
 	****************************************************
 	*/
-	$payReqMap['LGD_KVPMISPWAPURL']		 	= $LGD_KVPMISPWAPURL;	
-	$payReqMap['LGD_KVPMISPCANCELURL']  	= $LGD_KVPMISPCANCELURL;
-	
+	$payReqMap['LGD_KVPMISPWAPURL']			= $LGD_KVPMISPWAPURL;	
+	$payReqMap['LGD_KVPMISPCANCELURL']		= $LGD_KVPMISPCANCELURL;
 	/*
 	****************************************************
 	* 신용카드 ISP(국민/BC)결제에만 적용  - END
 	****************************************************
 	*/
+	
 		
 	/*
 	****************************************************
 	* 계좌이체 결제에만 적용 - BEGIN 
 	****************************************************
 	*/
-	$payReqMap['LGD_MTRANSFERWAPURL']		= $LGD_MTRANSFERWAPURL;	
+	$payReqMap['LGD_MTRANSFERWAPURL']  		= $LGD_MTRANSFERWAPURL;	
 	$payReqMap['LGD_MTRANSFERCANCELURL']  	= $LGD_MTRANSFERCANCELURL;
 	
 	/*
@@ -153,11 +159,12 @@
 	- iOS: N
 	- iOS일 경우, 반드시 N으로 값을 수정
 	*/
-	$payReqMap['LGD_KVPMISPAUTOAPPYN']	= "A";		// 신용카드 결제 
-	$payReqMap['LGD_MTRANSFERAUTOAPPYN']= "A";		// 계좌이체 결제
+	$payReqMap['LGD_KVPMISPAUTOAPPYN']		= "A";		// 신용카드 결제 
+	$payReqMap['LGD_MTRANSFERAUTOAPPYN']	= "A";		// 계좌이체 결제
+	
 
-    // 가상계좌(무통장) 결제연동을 하시는 경우  할당/입금 결과를 통보받기 위해 반드시 LGD_CASNOTEURL 정보를 토스페이먼츠에 전송해야 합니다 .
-    $payReqMap['LGD_CASNOTEURL'] = $LGD_CASNOTEURL;               // 가상계좌 NOTEURL
+    // 가상계좌(무통장) 결제연동을 하시는 경우  할당/입금 결과를 통보받기 위해 반드시 LGD_CASNOTEURL 정보를 LG 유플러스에 전송해야 합니다 .
+    $payReqMap['LGD_CASNOTEURL']			= $LGD_CASNOTEURL;               
 
     //Return URL에서 인증 결과 수신 시 셋팅될 파라미터 입니다.*/
     $payReqMap['LGD_RESPCODE']           = "";
@@ -167,6 +174,7 @@
 	$payReqMap['LGD_ENCODING_RETURNURL'] 		= "UTF-8";
 	$payReqMap['LGD_ENCODING_NOTEURL'] 			= "UTF-8";
 
+
     $_SESSION['PAYREQ_MAP'] = $payReqMap;
 ?>
 
@@ -175,15 +183,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>토스페이먼츠 eCredit서비스 결제테스트</title>
-<!-- test일 경우 -->
-<script language="javascript" src="https://pretest.tosspayments.com:9443/xpay/js/xpay_crossplatform.js" type="text/javascript"></script>
-<!-- 
-  service일 경우 아래 URL을 사용 
 <script language="javascript" src="https://xpay.tosspayments.com/xpay/js/xpay_crossplatform.js" type="text/javascript"></script>
- -->
 <script type="text/javascript">
 
-
+/*
+* iframe으로 결제창을 호출하시기를 원하시면 iframe으로 설정 (변수명 수정 불가)
+*/
 	var LGD_window_type = '<?= $CST_WINDOW_TYPE ?>'; 
 /*
 * 수정불가
@@ -231,7 +236,7 @@ function getFormObject() {
     </tr>    
     <tr>
         <td colspan="2">
-		<input type="button" value="인증요청" onclick="launchCrossPlatform();"/>         
+		<input type="button" value="인증요청" onClick="launchCrossPlatform();"/>         
         </td>
     </tr>    
 </table>
