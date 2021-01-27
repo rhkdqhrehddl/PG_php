2b7fb8c2ff83a78ad90d4eb2a22d9ff30f82abb8
rhkdqhrehddl <rhkdqhrehddl@gmail.com>
2021-01-27 09:14:42 +0900

    안심키인 모듈 추가

---
+++ AnsimKeyin/note_url.php
@@ -0,0 +1,124 @@
+<?php
+    $LGD_RESPCODE="";                         // 응답코드: 0000(성공) 그외 실패
+    $LGD_RESPMSG="";                          // 응답메세지
+    $LGD_HASHDATA="";                         // 해쉬값
+    $LGD_TID="";                              // 토스페이먼츠가 부여한 거래번호
+    $LGD_MID="";                              // 상점아이디 
+    $LGD_OID="";                              // 주문번호
+    $LGD_AMOUNT="";                           // 거래금액
+    $LGD_PAYTYPE="";                          // 결제수단코드
+    $LGD_PAYDATE="";                          // 거래일시(승인일시/이체일시)
+    $LGD_FINANCECODE="";                      // 결제기관코드(카드종류/은행코드)
+    $LGD_FINANCENAME="";                      // 결제기관이름(카드이름/은행이름)
+    $LGD_TIMESTAMP="";
+	$LGD_TAXFREEAMOUNT="";                    // 면세금액
+	
+    $LGD_FINANCEAUTHNUM="";                   // 승인번호(신용카드)
+    $LGD_CARDNUM="";                          // 카드번호(신용카드)
+    $LGD_CARDINSTALLMONTH="";                 // 할부개월수(신용카드)   
+    $LGD_CARDNOINTYN="";                      // 무이자할부여부(신용카드) - '1'이면 무이자할부 '0'이면 일반할부
+    $LGD_TRANSAMOUNT="";                      // 환율적용금액(신용카드)
+    $LGD_EXCHANGERATE="";                     // 환율(신용카드)
+
+    $LGD_ACCOUNTNUM="";                       // 계좌번호(계좌이체, 무통장입금) 
+
+    $LGD_PAYTELNUM="";                        // 휴대폰번호(휴대폰)
+
+    $LGD_CASFLAG="";                          // 무통장입금 플래그(무통장입금) - 'R':계좌할당, 'I':입금, 'C':입금취소
+    $LGD_CASTAMOUNT="";                       // 입금총액(무통장입금)
+    $LGD_CASCAMOUNT="";                       // 현입금액(무통장입금)
+    $LGD_CASSEQNO="";                         // 입금순서(무통장입금)
+    $LGD_CASHRECEIPTNUM="";                   // 현금영수증 승인번호
+
+    $LGD_CASHRECEIPTKIND="";                  // 현금영수증종류 (0: 소득공제용 , 1: 지출증빙용)
+    $LGD_CASHRECEIPTSELFYN="";                // 현금영수증자진발급제유무 Y: 자진발급제 적용, 그외 : 미적용
+    $LGD_ESCROWYN="";
+
+    /*
+     * OK캐쉬백
+     */
+    $LGD_OCBSAVEPOINT = "";                   // OK캐쉬백 적립포인트
+    $LGD_OCBTOTALPOINT = "";                  // OK캐쉬백 누적포인트
+    $LGD_OCBUSABLEPOINT = "";                 // OK캐쉬백 사용가능 포인트
+
+    $LGD_RESPCODE = $_POST["LGD_RESPCODE"];
+    $LGD_RESPMSG = $_POST["LGD_RESPMSG"];
+    $LGD_HASHDATA = $_POST["LGD_HASHDATA"];
+    $LGD_TID = $_POST["LGD_TID"];
+    $LGD_MID = $_POST["LGD_MID"];
+    $LGD_OID = $_POST["LGD_OID"];
+    $LGD_AMOUNT = $_POST["LGD_AMOUNT"];
+    $LGD_PAYTYPE = $_POST["LGD_PAYTYPE"];
+    $LGD_PAYDATE = $_POST["LGD_PAYDATE"];
+    $LGD_FINANCECODE = $_POST["LGD_FINANCECODE"];
+    $LGD_FINANCENAME = $_POST["LGD_FINANCENAME"];
+    $LGD_FINANCEAUTHNUM = $_POST["LGD_FINANCEAUTHNUM"];
+    $LGD_CARDNUM = $_POST["LGD_CARDNUM"];
+    $LGD_CARDNOINTYN = $_POST["LGD_CARDNOINTYN"];
+    $LGD_TRANSAMOUNT = $_POST["LGD_TRANSAMOUNT"];
+    $LGD_EXCHANGERATE = $_POST["LGD_EXCHANGERATE"];
+    $LGD_ACCOUNTNUM = $_POST["LGD_ACCOUNTNUM"];
+    $LGD_PAYTELNUM = $_POST["LGD_PAYTELNUM"];
+    $LGD_CASFLAG = $_POST["LGD_CASFLAG"];
+    $LGD_CASTAMOUNT = $_POST["LGD_CASTAMOUNT"];
+    $LGD_CASCAMOUNT = $_POST["LGD_CASCAMOUNT"];
+    $LGD_CASSEQNO= $_POST["LGD_CASSEQNO"];
+    $LGD_CASHRECEIPTNUM= $_POST["LGD_CASHRECEIPTNUM"];
+    $LGD_CASHRECEIPTKIND= $_POST["LGD_CASHRECEIPTKIND"];
+    $LGD_CASHRECEIPTSELFYN= $_POST["LGD_CASHRECEIPTSELFYN"];
+    $LGD_ESCROWYN= $_POST["LGD_ESCROWYN"];
+    $LGD_TAXFREEAMOUNT= $_POST["LGD_TAXFREEAMOUNT"];
+
+    $LGD_CARDINSTALLMONTH = $_POST["LGD_CARDINSTALLMONTH"];
+    $LGD_TIMESTAMP = $_POST["LGD_TIMESTAMP"];
+
+    $LGD_OCBSAVEPOINT= $_POST["LGD_OCBSAVEPOINT"];
+    $LGD_OCBTOTALPOINT= $_POST["LGD_OCBTOTALPOINT"];
+    $LGD_OCBUSABLEPOINT= $_POST["LGD_OCBUSABLEPOINT"];
+
+
+   $configPath                 = "C:/lgdacom";                                  //토스페이먼츠에서 제공한 환경파일("/conf/lgdacom.conf") 위치 지정.     
+	
+	if(PHP_OS === "Linux"){
+		$configPath             = "/lgdacom";
+	}
+	
+	require_once($configPath . "/XPayClient.php");
+    $xpay = new XPayClient($configPath, $CST_PLATFORM);
+   	$xpay->Init_TX($LGD_MID);
+	
+    $LGD_HASHDATA2 = $xpay->GetHashDataCas($LGD_MID,$LGD_OID,$LGD_AMOUNT,$LGD_RESPCODE,$LGD_TIMESTAMP);
+
+    /*
+     * 상점 처리결과 리턴메세지
+     *
+     * OK   : 상점 처리결과 성공
+     * 그외 : 상점 처리결과 실패
+     *
+     * ※ 주의사항 : 성공시 'OK' 문자이외의 다른문자열이 포함되면 실패처리 되오니 주의하시기 바랍니다.
+     */    
+    $resultMSG = "결제결과 상점 DB처리 결과값을 입력해 주시기 바랍니다.";
+	  
+    if ($LGD_HASHDATA2 == $LGD_HASHDATA) {      //해쉬값 검증이 성공하면
+        if($LGD_RESPCODE == "0000"){            //결제가 성공이면
+            /*
+             * 거래성공 결과 상점 처리(DB) 부분
+             * 상점 결과 처리가 정상이면 "OK"
+             */    
+            if( 결제성공 상점처리결과 성공 ) resultMSG = "OK";   
+        }else {                                 //결제가 실패이면
+            /*
+             * 거래실패 결과 상점 처리(DB) 부분
+             * 상점결과 처리가 정상이면 "OK"
+             */  
+           if( 결제실패 상점처리결과 성공 ) resultMSG = "OK";    
+        }
+    } else {                                    //해쉬값 검증이 실패이면
+        /*
+         * hashdata검증 실패 로그를 처리하시기 바랍니다. 
+         */  
+		$resultMSG = "결제결과 상점 DB처리 해쉬값 검증이 실패하였습니다.";         
+    }
+
+    echo $resultMSG;        
+?>
---
+++ AnsimKeyin/pay_req.php
@@ -0,0 +1,100 @@
+<?
+    /*
+     * 1. 기본결제정보 변경
+     *
+     * 결제기본정보를 변경하여 주시기 바랍니다.     
+     */
+     $CST_PLATFORM     = "test";     							//토스페이먼츠 결제 서비스 선택(test:테스트, service:서비스)
+     $CST_MID          = "mk_KVP_selex";           							//상점아이디(토스페이먼츠으로 부터 발급받으신 상점아이디를 입력하세요)
+                                                                //테스트 아이디는 't'를 반드시 제외하고 입력하세요.
+     $LGD_MID  = (("test" == $CST_PLATFORM)?"t":"").$CST_MID;   //상점아이디(자동생성)
+
+	 $LGD_OID          = "test_0001";							//[반드시 세팅]주문번호(상점정의 유니크한 주문번호를 입력하세요)
+     $LGD_AMOUNT       = "1004"	;								//결제금액("," 를 제외한 결제금액을 입력하세요)    
+     $LGD_BUYER        = "홍길동";								//구매자명
+     $LGD_PRODUCTINFO  = "테스트상품";							//상품명 
+     $LGD_NOTEURL      = "";               						//상점결제결과 처리(DB) 페이지 URL (URL을 입력해 주세요. 예: http://pgweb.tosspayments.com/note_url.php)	
+     $LGD_TAXFREEAMOUNT= "";									//면세금액 (면세상품취급 상점의 경우 사용 - 사용전 "부분면세" 계약을 확인하세요)
+
+?>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>결제요청</title>
+<script type="text/javascript">
+
+/*
+ * 결제요청 및 결과화면 처리 
+ */
+function doAnsimKeyin(){
+//CST_PLATFORM은 test(테스트) 또는 service(서비스)를 넘겨주시면 됩니다. 
+    ret = ansimkeyin_check(document.getElementById('LGD_PAYINFO'), '<?= $CST_PLATFORM ?>'); 
+
+    if (ret=="00"){ //plug-in 정상 로딩
+        var LGD_RESPCODE       = dpop.getData('LGD_RESPCODE');       //결과코드
+        var LGD_RESPMSG        = dpop.getData('LGD_RESPMSG');        //결과메세지
+	      if( "0000" == LGD_RESPCODE ) { //결제성공 
+            var LGD_TID            = dpop.getData('LGD_TID');            //토스페이먼츠 거래KEY
+	          var LGD_PAYDATE        = dpop.getData('LGD_PAYDATE');        //결제일자
+            var LGD_FINANCECODE    = dpop.getData('LGD_FINANCECODE');    //결제기관코드
+            var LGD_FINANCENAME    = dpop.getData('LGD_FINANCENAME');    //결제기관이름
+            var LGD_NOTEURL_RESULT = dpop.getData('LGD_NOTEURL_RESULT'); //상점DB처리결과
+
+            alert("결제가 성공하였습니다. 토스페이먼츠 거래ID : " + LGD_TID);
+            /*
+             * 결제성공 화면 처리
+             */        
+       
+        } else { //결제실패
+             alert("결제가 실패하였습니다. " + LGD_RESPMSG);
+            /*
+             * 결제실패 화면 처리
+             */        
+        }
+    } else {
+        alert("토스페이먼츠 전자결제를 위한 ActiveX 설치 실패");
+    }     
+}
+
+        
+</script>
+</head>
+
+<body>
+<form method="post" id="LGD_PAYINFO">
+<input type="hidden" name="LGD_MID"             value="<?= $LGD_MID ?>">									<!-- 상점아이디 -->
+<input type="hidden" name="LGD_NOTEURL"         value="<?= $LGD_NOTEURL ?>">			<!-- 결제결과처리_URL(LGD_NOTEURL) -->
+<table>
+    <tr>
+        <td>주문번호 </td>
+        <td><input type="text" name="LGD_OID"     value="<?= $LGD_OID ?>">	<!-- 주문번호 --></td>
+    </tr>
+    <tr>
+        <td>상품정보 </td>
+        <td><input type="text" name="LGD_PRODUCTINFO"     value="<?= $LGD_PRODUCTINFO ?>">		<!-- 상품정보 --></td>
+    </tr>
+    <tr>
+        <td>결제금액 </td>
+        <td><input type="text" name="LGD_AMOUNT"  value="<?= $LGD_AMOUNT ?>">		<!-- 결제금액 --></td>
+    </tr>
+    <tr>
+        <td>구매자명 </td>
+        <td><input type="text" name="LGD_BUYER"   value="<?= $LGD_BUYER ?>">	<!-- 구매자 --></td>
+    </tr>
+        
+    <tr>
+        <td colspan="2">* 추가 상세 결제요청 파라미터는 메뉴얼을 참조하세요.</td>
+    </tr>
+    <tr>
+        <td>
+        <input type="button" value="결제요청(ActiveX)" onclick="doAnsimKeyin()"/><br>
+        </td>
+    </tr>
+</table>
+</form>
+</body>
+<!--  xpay.js는 반드시 body 밑에 두시기 바랍니다. -->
+<script language="javascript" src="http://xpay.tosspayments.com/ansim-keyin/js/ansim-keyin.js" type="text/javascript"></script>
+</script>
+</html>
