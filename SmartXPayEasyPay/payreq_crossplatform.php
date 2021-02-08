0f85c238ea658d6397d5d62039d806b787102c0c
rhkdqhrehddl <rhkdqhrehddl@gmail.com>
2021-02-08 13:34:44 +0900

    삼성페이 간편결제 테스트

--- SmartXPayEasyPay/payreq_crossplatform.php
+++ SmartXPayEasyPay/payreq_crossplatform.php
@@ -117,7 +117,7 @@
     $payReqMap['LGD_EASYPAY_ONLY']			= $LGD_EASYPAY_ONLY;                    // paynow 사용여부
     $payReqMap['LGD_MONEPAY_AUTORUNYN']		= $LGD_MONEPAY_AUTORUNYN;				// paynow 자동구동 여부
 	$payReqMap['LGD_PCVIEWYN']				= $LGD_PCVIEWYN;						// 휴대폰번호 입력 화면 사용 여부(유심칩이 없는 단말기에서 입력-->유심칩이 있는 휴대폰에서 실제 결제)
-
+	$payReqMap['LGD_PAYDIRECT']  			= "Y";
 
 	//iOS 연동시 필수
 	$payReqMap['LGD_MPILOTTEAPPCARDWAPURL'] = $LGD_MPILOTTEAPPCARDWAPURL;
